2430b32b73f85bf1a7dc783180ad2479062fda7a
Anton <m@antonz.ru>
2020-04-21 01:39:46 +0300

    feat: Yandex.Money schema

--- iuliia/__init__.py
+++ iuliia/__init__.py
@@ -3,8 +3,17 @@ Transliterate Cyrillic → Latin in all ways possible
 """
 
 # flake8: noqa
+from enum import Enum
 from iuliia.engine import translate
 from iuliia.schema import Schema
+from iuliia.yandexmoney import YANDEX_MONEY
 
-__version__ = "0.2.1"
+
+class Schemas(Enum):
+    """All supported transliteration schemas."""
+
+    yandex_money = YANDEX_MONEY
+
+
+__version__ = "0.3.0"
 __all__ = []
---
+++ iuliia/yandexmoney.py
@@ -0,0 +1,44 @@
+"""
+Yandex.Money transliteration schema.
+https://dangry.ru/iuliia/yandex-money/
+"""
+
+from .schema import Schema
+
+MAPPING = {
+    "а": "a",
+    "б": "b",
+    "в": "v",
+    "г": "g",
+    "д": "d",
+    "е": "e",
+    "ё": "e",
+    "ж": "zh",
+    "з": "z",
+    "и": "i",
+    "й": "i",
+    "к": "k",
+    "л": "l",
+    "м": "m",
+    "н": "n",
+    "о": "o",
+    "п": "p",
+    "р": "r",
+    "с": "s",
+    "т": "t",
+    "у": "u",
+    "ф": "f",
+    "х": "kh",
+    "ц": "ts",
+    "ч": "ch",
+    "ш": "sh",
+    "щ": "sch",
+    "ъ": "",
+    "ы": "y",
+    "ь": "",
+    "э": "e",
+    "ю": "yu",
+    "я": "ya",
+}
+
+YANDEX_MONEY = Schema(MAPPING)
