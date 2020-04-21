091c3a1e8ee35feba2572e70ff8ae4d57f9b35a9
Anton <m@antonz.ru>
2020-04-21 06:35:23 +0300

    refactor: less duplication in mappings

--- iuliia/wikipedia.py
+++ iuliia/wikipedia.py
@@ -3,42 +3,26 @@ Wikipedia transliteration schema.
 https://dangry.ru/iuliia/wikipedia/
 """
 
-from .schema import Schema
+from .schema import Schema, BASE_MAPPING
 
 MAPPING = {
-    "а": "a",
-    "б": "b",
-    "в": "v",
-    "г": "g",
-    "д": "d",
-    "е": "e",
-    "ё": "yo",
-    "ж": "zh",
-    "з": "z",
-    "и": "i",
-    "й": "y",
-    "к": "k",
-    "л": "l",
-    "м": "m",
-    "н": "n",
-    "о": "o",
-    "п": "p",
-    "р": "r",
-    "с": "s",
-    "т": "t",
-    "у": "u",
-    "ф": "f",
-    "х": "kh",
-    "ц": "ts",
-    "ч": "ch",
-    "ш": "sh",
-    "щ": "shch",
-    "ъ": "",
-    "ы": "y",
-    "ь": "",
-    "э": "e",
-    "ю": "yu",
-    "я": "ya",
+    **BASE_MAPPING,
+    **{
+        "ё": "yo",
+        "ж": "zh",
+        "й": "y",
+        "х": "kh",
+        "ц": "ts",
+        "ч": "ch",
+        "ш": "sh",
+        "щ": "shch",
+        "ъ": "",
+        "ы": "y",
+        "ь": "",
+        "э": "e",
+        "ю": "yu",
+        "я": "ya",
+    },
 }
 
 PREV_MAPPING = {
--- iuliia/yandexmoney.py
+++ iuliia/yandexmoney.py
@@ -3,42 +3,26 @@ Yandex.Money transliteration schema.
 https://dangry.ru/iuliia/yandex-money/
 """
 
-from .schema import Schema
+from .schema import Schema, BASE_MAPPING
 
 MAPPING = {
-    "а": "a",
-    "б": "b",
-    "в": "v",
-    "г": "g",
-    "д": "d",
-    "е": "e",
-    "ё": "e",
-    "ж": "zh",
-    "з": "z",
-    "и": "i",
-    "й": "i",
-    "к": "k",
-    "л": "l",
-    "м": "m",
-    "н": "n",
-    "о": "o",
-    "п": "p",
-    "р": "r",
-    "с": "s",
-    "т": "t",
-    "у": "u",
-    "ф": "f",
-    "х": "kh",
-    "ц": "ts",
-    "ч": "ch",
-    "ш": "sh",
-    "щ": "sch",
-    "ъ": "",
-    "ы": "y",
-    "ь": "",
-    "э": "e",
-    "ю": "yu",
-    "я": "ya",
+    **BASE_MAPPING,
+    **{
+        "ё": "e",
+        "ж": "zh",
+        "й": "i",
+        "х": "kh",
+        "ц": "ts",
+        "ч": "ch",
+        "ш": "sh",
+        "щ": "sch",
+        "ъ": "",
+        "ы": "y",
+        "ь": "",
+        "э": "e",
+        "ю": "yu",
+        "я": "ya",
+    },
 }
 
 YANDEX_MONEY = Schema(MAPPING)
